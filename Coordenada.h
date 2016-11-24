b8d915793f4bd0db1664ea32c46a7686b07b6c9e
Leandro Vigali <levleandro@gmail.com>
2016-11-24 14:02:12 -0300

    nada

--- Coordenada.h
+++ Coordenada.h
@@ -7,15 +7,13 @@ class Coordenada {
 
 	public:
 	
-	~Coordenada();
-
 	Coordenada(aed2::Nat lat, aed2::Nat lon);
 
-	const aed2::Nat Latitud() const;
+	aed2::Nat Latitud() const;
 
-	const aed2::Nat Longitud() const;
+	aed2::Nat Longitud() const;
 
-	const aed2::Nat Distancia(const Coordenada & c1, const Coordenada & c2) const;
+	aed2::Nat Distancia(const Coordenada & c1, const Coordenada & c2) const;
 
 	bool operator == (const Coordenada& c2) const;
 
@@ -27,19 +25,15 @@ class Coordenada {
 
 Coordenada::Coordenada(aed2::Nat lat, aed2::Nat lon) : latitud(lat), longitud(lon){}
 
-Coordenada::~Coordenada(){
-
-}
-
-const aed2::Nat Coordenada::Latitud() const{
+aed2::Nat Coordenada::Latitud() const{
 	return latitud;
 }
 
-const aed2::Nat Coordenada::Longitud() const{
+aed2::Nat Coordenada::Longitud() const{
 	return longitud;
 }
 
-const aed2::Nat Coordenada::Distancia(const Coordenada & c1, const Coordenada & c2) const{
+aed2::Nat Coordenada::Distancia(const Coordenada & c1, const Coordenada & c2) const{
 	aed2::Nat n1;
 	aed2::Nat n2;
 	if (c1.latitud > c2.latitud)
@@ -58,7 +52,7 @@ const aed2::Nat Coordenada::Distancia(const Coordenada & c1, const Coordenada & 
 
 }
 
-bool Coordenada::operator == (const Coordenada& c2)const{
+bool Coordenada::operator == (const Coordenada& c2) const{
  	return (latitud == c2.latitud) and (longitud == c2.longitud);
 }
 
